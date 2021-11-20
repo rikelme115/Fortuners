18b46ca19d63e56899b685c308174efba3f7f9a4
Ricardo Andres Londoño <rikelme115@gmail.com>
2021-11-19 23:18:39 -0500

    RALM PointViewAdapter

--- app/src/main/java/com/example/fortuners/MainActivity.kt
+++ app/src/main/java/com/example/fortuners/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.fortuners
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.*
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,51 +21,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = PointViewAdapter(getPoints())
     }
 
-    class PointViewAdapter(private val values: List<Point>) :
-        RecyclerView.Adapter<PointViewAdapter.ViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_content, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val point = values[position]
-            holder.nameTextView.text = point.name
-            holder.dateTextView.text = point.date
-            holder.rakingTextView.text = point.puntuacion
-
-
-            holder.posterImageView.let {
-                Glide.with(holder.itemView.context)
-                    .load(point.url)
-                    .into(it)
-            }
-
-            holder.itemView.setOnClickListener { v ->
-                val intent = Intent(v.context, DetailActivity::class.java).apply {
-                    putExtra("id", point.id)
-                    putExtra("name", point.name)
-                    putExtra("description", point.description)
-                    putExtra("url", point.url)
-                    putExtra("ubicacion", point.ubicacion)
-                    putExtra("temperatura", point.temperatura)
-                }
-                v.context.startActivity(intent)
-            }
-
-        }
-
-        override fun getItemCount() = values.size
-
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val nameTextView: TextView = view.findViewById(R.id.nameTextView)
-            val dateTextView: TextView = view.findViewById(R.id.dateTextView)
-            val posterImageView: ImageView = view.findViewById(R.id.posterImageView)
-            val rakingTextView: TextView = view.findViewById((R.id.rakingTextView))
-        }
-    }
 
     private fun getPoints(): MutableList<Point>{
         val mPoint:MutableList<Point> = ArrayList()
---
+++ app/src/main/java/com/example/fortuners/PointViewAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.fortuners
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class PointViewAdapter(private val values: List<Point>) : RecyclerView.Adapter<PointViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_content, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val point = values[position]
+        holder.nameTextView.text = point.name
+        holder.dateTextView.text = point.date
+        holder.rakingTextView.text = point.puntuacion
+
+
+        holder.posterImageView.let {
+            Glide.with(holder.itemView.context)
+                .load(point.url)
+                .into(it)
+        }
+
+        holder.itemView.setOnClickListener { v ->
+            val intent = Intent(v.context, DetailActivity::class.java).apply {
+                putExtra("id", point.id)
+                putExtra("name", point.name)
+                putExtra("description", point.description)
+                putExtra("url", point.url)
+                putExtra("ubicacion", point.ubicacion)
+                putExtra("temperatura", point.temperatura)
+            }
+            v.context.startActivity(intent)
+        }
+
+    }
+
+    override fun getItemCount() = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nameTextView: TextView = view.findViewById(R.id.nameTextView)
+        val dateTextView: TextView = view.findViewById(R.id.dateTextView)
+        val posterImageView: ImageView = view.findViewById(R.id.posterImageView)
+        val rakingTextView: TextView = view.findViewById((R.id.rakingTextView))
+    }
+}
